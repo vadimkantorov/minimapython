4340f9a367a609b906430eb1820fa8107f226ee9
Vadim Kantorov <vadimkantorov@gmail.com>
2024-05-08 15:43:47 +0200

    ...

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 python minimapython.py -c _config.json --sitemap-path sitemap.xml -i index.md                                 -o _site/index.html --baseurl /blog --siteurl http://localhost:8000
-python minimapython.py -c _config.json --sitemap-path sitemap.xml -i about.md                                 -o _site/about.html
+#python minimapython.py -c _config.json --sitemap-path sitemap.xml -i about.md                                 -o _site/about.html
 
-python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-19-super-short-article.md -o _site/blog/2016-05-19-super-short-article.html
-python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-my-example-post.md     -o _site/blog/2016-05-20-my-example-post.html
-python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-super-long-article.md  -o _site/blog/2016-05-20-super-long-article.html
-python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-this-post-demonstrates-poscontent-styles.md -o ./_site/blog/2016-05-20-this-post-demonstrates-post-content-styles.html
-python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-welcome-to-jekyll.md   -o _site/blog/2016-05-20-welcome-to-jekyll.html
+#python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-19-super-short-article.md -o _site/blog/2016-05-19-super-short-article.html
+#python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-my-example-post.md     -o _site/blog/2016-05-20-my-example-post.html
+#python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-super-long-article.md  -o _site/blog/2016-05-20-super-long-article.html
+#python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-this-post-demonstrates-poscontent-styles.md -o ./_site/blog/2016-05-20-this-post-demonstrates-post-content-styles.html
+#python minimapython.py -c _config.json --sitemap-path sitemap.xml -i _posts/2016-05-20-welcome-to-jekyll.md   -o _site/blog/2016-05-20-welcome-to-jekyll.html
